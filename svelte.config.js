07e9617bbdbf914d251e0e944b0fe7c558751ad6
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-10-23 22:29:49 +0300

    Oops

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
-import { Config } from '@sveltejs/kit'
-
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	preprocess: preprocess({
     postcss: true
   }),
 	kit: {
 		target: '#svelte',
 		adapter: adapter({
-			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null
